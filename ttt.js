1af3003e0ceed3393bb30cd796871100cc997f94
chinX22 <eliastessema33@gmail.com>
2024-07-06 06:03:34 -0500

    better game, ui looks better as well

--- ttt.js
+++ ttt.js
@@ -1,70 +1,120 @@
 function player (){
     this.moves = [];
-    const addMove = (place) => moves.push(place);
 }
 
-function getButtons(){
-    let buttonArr = [];
-    console.log("hey");
+function makeBoard(){
+    let board = document.querySelector(".board")
     for(let i = 0; i < 9; i++){
-        buttonArr[i] = document.querySelector(".but" + (i+1))
-        buttonArr[i].addEventListener("click", () => {console.log("hi")});
+        let place = document.createElement("button");
+        place.setAttribute("id", "but" + (i+1));
+        board.appendChild(place);
     }
 }
 
-function takeTurns(x){
-    if((turn % 2) != 0){
-        player1.moves.push(x);        
-    } else {
-        player2.moves.push(x);
-    }
+function clearBoard(){
+    let board = document.querySelector(".board");
+    board.replaceChildren();
 }
 
-function playGame(){
-    const player1 = new player();
-    const player2 = new player();
-    const resetButton = document.querySelector("#clear");
+function setNames(p1Element, p2Element){
+    const myDialog = document.querySelector("#names");
+    const myForm = document.querySelector("form");
+    p1Element.textContent = "Player 1";
+    p2Element.textContent = "Player 2";
+    myDialog.showModal();
+    myForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        myDialog.close();
+    let player1name = myForm.elements['player1name'].value;
+    let player2name = myForm.elements['player2name'].value;
+    if(player1name != ""){
+    p1Element.textContent = player1name;
+    }
+    if(player2name != ""){
+        p2Element.textContent = player2name;
+    }
+    })
+}
+
+
+    
+
+function gameMaster(){
+    let player1 = new player();
+    let player2 = new player();
+    let win = 0;     
+    const winDialog = document.querySelector("#winner");
+    winDialog.close();
+    const gameOverEvent = new Event("gameOver");
+    const p1nameElement = document.querySelector("#player1");
+    const p2nameElement = document.querySelector("#player2");
+    setNames(p1nameElement, p2nameElement);
+   
+    const winnerText = document.querySelector("#winnertext")
+    document.addEventListener("gameOver", () => {
+        winnerDisplay(winDialog, winnerText, p1nameElement.textContent, p2nameElement.textContent)
+        });
+
+    const resetButton = document.querySelector("#reset");
     resetButton.addEventListener("click", () => {
+        winDialog.close();
         reset();
     });
+    const clearButton = document.querySelector("#clear");
+    clearButton.addEventListener("click", () => {reset();});
+    
     let winningArrays = makeWinningArray();
     let buttonArr = [];
-    console.log("hey");
     let turn = 1;
-    let win = 0;
+    playGame();
 
-    for(let i = 0; i < 9; i++){
-        buttonArr[i] = document.querySelector("#but" + (i+1))
-        buttonArr[i].addEventListener("click", () => {
-            if(win == 0){
-                if((turn % 2) != 0){
-                    player1.moves.push(i+1); 
-                    buttonArr[i].classList.add("x");
-                } else {
-                    player2.moves.push(i+1);
-                    buttonArr[i].classList.add("o");
+    function playGame(){
+        win = 0;
+        turn = 1;
+       makeBoard(); 
+        for(let i = 0; i < 9; i++){
+            buttonArr[i] = document.querySelector("#but" + (i+1))
+            buttonArr[i].addEventListener("click", () => {
+                if(win == 0 && 
+                    (!(buttonArr[i].classList.contains("x"))
+                    && !(buttonArr[i].classList.contains("o")))){
+                    if((turn % 2) != 0){
+                        player1.moves.push(i+1); 
+                        !buttonArr[i].classList.add("x");
+                    } else {
+                        player2.moves.push(i+1);
+                        buttonArr[i].classList.add("o");
+                    }
+                    turn++;
+                    if(turn > 2){
+                        checkWinner(player1, player2, winningArrays);
+                        if(win != 0){
+                            document.dispatchEvent(gameOverEvent);
+                        }
+                    };
                 }
-                console.log(player1.moves);
-                console.log(player2.moves);
-                turn++;
-                if(turn > 2){
-                    win = checkWinner(player1, player2, winningArrays);
-                };
-            }
-        });
+            });
+        
+        }
     }
     
+    function winnerDisplay(dialog, text, p1name, p2name){
+        dialog.style.visibility = 'visible';
+        
+        text.textContent = (win == 1) ? "Winner is: " + p1name : "Winner is: " + p2name;
+    }
+
     function reset(){
+        winDialog.style.visibility = 'hidden';
         for(box of buttonArr){
             box.classList.remove(...box.classList);
         }
         player1.moves = [];
         player2.moves = [];
+        clearBoard();
         playGame();
     }
-    
-}
-
+   
     function checkWinner(p1, p2, arrs){
         let winningArrays = arrs;
             if(p1.moves.length > 2){ 
@@ -78,11 +128,8 @@ function playGame(){
                         }
                     }
                     if (count > 2){
-                        console.log("p1 win");
-                        return 1;
-                    }
-                }
-            }
+                        win = 1;
+                    }}}
 
             if(p2.moves.length > 2){ 
                 for (let i = 0; i < winningArrays.length; i++){
@@ -95,29 +142,34 @@ function playGame(){
                         }
                     }
                     if (count > 2){
-                        console.log("p2 win");
-                        return 2;
-                    }
-                }
-            }
+                        win = 2;
+                    }}}
 
             return 0;
         }
+    }
+
+
+
+    
         
 
 
 function makeWinningArray(){
-    let here = [];
-    let a = [1,2,3];
-    let b = [1,4,7];
-    here.push(a);
-    here.push(a.map((x) => x+ 3));
-    here.push(a.map((x) => x+ 6));
-    here.push(b);
-    here.push(b.map((x) => x+ 1));
-    here.push(b.map((x) => x+ 2));
-    here.push([1,5,9]);
-    here.push([3,5,7]);
+    //let here = [];
+    // let a = [1,2,3];
+    // let b = [1,4,7];
+    // here.push(a);
+    // here.push(a.map((x) => x+ 3));
+    // here.push(a.map((x) => x+ 6));
+    // here.push(b);
+    // here.push(b.map((x) => x+ 1));
+    // here.push(b.map((x) => x+ 2));
+    // here.push([1,5,9]);
+    // here.push([3,5,7]);
+    let here =[
+        [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
+    ]
 
     return here;
 }
